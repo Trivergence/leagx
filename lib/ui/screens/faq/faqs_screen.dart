7f789f47b14577a27a0a976769cd4b4c517065bc
Ahsanwarsi <ahsan.ue@gmail.com>
2022-08-23 18:16:10 +0500

    added faqs

--- lib/ui/screens/faq/faqs_screen.dart
+++ lib/ui/screens/faq/faqs_screen.dart
@@ -35,36 +35,58 @@ class FaqsScreen extends StatelessWidget {
           ),
         ),
         child: Column(
-          children: [
+          children:  [
             const GradientWidget(
               child: TextWidget(
-                text: 'How Lorem ipsum dolor  ',
+                text: 'Frequently Asked Questions',
                 fontWeight: FontWeight.w600,
                 textSize: 20.0,
               ),
             ),
             UIHelper.verticalSpaceMedium,
             Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return const FaqTile(
-                    title: ' Vestibulum ex odio, vehicula eu?',
-                    subtitle:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat turpis, congue eu interdum non, rhoncus quis massa. ',
-                  );
-                },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: const [
+                    FaqTile(
+                      title: 'Lorem ipsum dolor sit amet?',
+                    ),
+                    FaqTile(
+                      title: ' Vestibulum ex odio, vehicula eu?',
+                      subtitle:
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat turpis, congue eu interdum non, rhoncus quis massa. ',
+                    ),
+                    FaqTile(
+                      title:
+                      'Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem.',
+                      subtitle:
+                      'Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat turpis, congue eu interdum non, rhoncus quis massa. ',
+                    ),
+                    FaqTile(
+                      title: 'Phasellus efficitur in lorem nec molestie',
+                      subtitle:
+                      'Phasellus erat turpis, congue eu interdum non, rhoncus quis massa.Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat turpis, congue eu interdum non, rhoncus quis massa.',
+                    ),
+                    FaqTile(
+                      title:
+                      'Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem.',
+                      subtitle:
+                      'Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat turpis, congue eu interdum non, rhoncus quis massa. ',
+                    ),
+                    FaqTile(
+                      title: 'Phasellus efficitur in lorem nec molestie',
+                      subtitle:
+                      'Phasellus erat turpis, congue eu interdum non, rhoncus quis massa.Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat turpis, congue eu interdum non, rhoncus quis massa.',
+                    ),
+                    FaqTile(
+                      title: 'Ut varius sagittis arcu et sollicitudin?',
+                      subtitle:
+                      'Phasellus efficitur in lorem nec molestie. Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem.Nunc sem ex, mattis sit amet nisi ut, laoreet scelerisque sem.',
+                    ),
+                  ],
+                ),
               ),
             ),
-            // FaqTile(
-            //   title: 'Lorem ipsum dolor sit amet?',
-            // ),
-            // FaqTile(
-            //   title: ' Vestibulum ex odio, vehicula eu?',
-            //   subtitle:
-            //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat turpis, congue eu interdum non, rhoncus quis massa. ',
-            // ),
           ],
         ),
       ),
