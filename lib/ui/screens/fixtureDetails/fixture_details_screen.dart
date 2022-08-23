fbc342851f402143681fdcb213a3fbbffd03f6ec
Ahsanwarsi <ahsan.ue@gmail.com>
2022-08-23 17:25:19 +0500

    implement news view in fixture details screen

--- lib/ui/screens/fixtureDetails/fixture_details_screen.dart
+++ lib/ui/screens/fixtureDetails/fixture_details_screen.dart
@@ -1,21 +1,11 @@
 import 'package:bailbooks_defendant/constants/colors.dart';
-import 'package:bailbooks_defendant/constants/dimens.dart';
-import 'package:bailbooks_defendant/ui/util/ui/ui_helper.dart';
+import 'package:bailbooks_defendant/ui/screens/fixtureDetails/news/news_view.dart';
 import 'package:bailbooks_defendant/ui/widgets/app_bar_widget.dart';
-import 'package:bailbooks_defendant/ui/widgets/divider_widget.dart';
-import 'package:bailbooks_defendant/ui/widgets/gradient_widget.dart';
 import 'package:bailbooks_defendant/ui/widgets/header_widget.dart';
-import 'package:bailbooks_defendant/ui/widgets/main_button.dart';
-import 'package:bailbooks_defendant/ui/widgets/text_widget.dart';
-import 'package:flutter/material.dart';
 
-import '../../../constants/assets.dart';
-import '../../util/size/size_config.dart';
+import 'package:flutter/material.dart';
 import '../../util/ui_model/tab_button_model.dart';
-import '../../widgets/icon_container.dart';
-import 'components/detail_tile.dart';
-import 'components/score_chip.dart';
-import 'match/components/live_match_widget.dart';
+
 import 'match/match_view.dart';
 import 'players/players_view.dart';
 
@@ -56,8 +46,8 @@ class _FixtureDetailsState extends State<FixtureDetails> {
           });
         }),
         
-       index == 0 ? MatchView()
-       : index == 1 ? PlayersView() : SizedBox.shrink(),
+       index == 0 ?const  MatchView()
+       : index == 1 ? PlayersView(): index==2? const NewsView() : const SizedBox.shrink(),
     ],),
     );
   }
---
+++ lib/ui/screens/fixtureDetails/news/news_view.dart
@@ -0,0 +1,23 @@
+import 'package:bailbooks_defendant/ui/widgets/news_tile.dart';
+import 'package:flutter/material.dart';
+class NewsView extends StatelessWidget {
+  const NewsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: 10,
+          padding: const EdgeInsets.only(top: 20),
+          itemBuilder: (_, index) {
+            return const NewsTile(
+              imageUrl: 'https://i.pravatar.cc/300',
+              postedBy: 'James FC',
+              when: '1 min ago',
+              desc: 'FIFA’s iconic competitions inspire billions of football fans and provide opportunities to have a wider positive social and environmental impact. By the global nature of the tournaments it ...',
+            );
+          }),
+    );
+  }
+}
