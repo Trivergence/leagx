2feae89c369e96308ff84c022ceccfb9c7512739
Ahsanwarsi <ahsan.ue@gmail.com>
2022-09-13 18:51:15 +0500

    implement: twitter login

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -13,5 +13,12 @@ class Strings {
 
   // exception and error messages
   static const String noInternet = "No Internet";
-  static const String badHappened = "Somethig bad happened occured, please try again later";    
+  static const String badHappened = "Somethig bad happened occured, please try again later";   
+
+  //keys
+  static const String apiKeyTwitter = "i3ZQyWHc6vD7yyUZuYiookqAK";
+  static const String apiSecretKeyTwitter = "a7B3JE1V3zV3m0yhLPRjQnhtwEsN5d7rpElpHFIri7Gmf1nCUD"; 
+
+  //uri
+  static const String redirectUriTwitter = "leagx://";
 }
