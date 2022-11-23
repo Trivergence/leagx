840c3f9e1df0a6c7082fd810d0725fafd360def9
Ahsanwarsi <ahsan.ue@gmail.com>
2022-11-23 16:09:50 +0500

    Fixture: Fixed -> Scrolling subscribed list of leagues

--- lib/ui/screens/dashboard/components/fixture/fixture.dart
+++ lib/ui/screens/dashboard/components/fixture/fixture.dart
@@ -1,6 +1,5 @@
 import 'package:leagx/constants/assets.dart';
 import 'package:leagx/constants/colors.dart';
-import 'package:leagx/constants/dimens.dart';
 import 'package:leagx/core/utility.dart';
 import 'package:leagx/models/subscribed_league.dart';
 import 'package:leagx/routes/routes.dart';
@@ -8,7 +7,6 @@ import 'package:leagx/ui/screens/dashboard/components/fixture/components/league_
 import 'package:leagx/ui/util/locale/localization.dart';
 import 'package:leagx/ui/util/ui/ui_helper.dart';
 import 'package:leagx/ui/screens/dashboard/components/fixture_widget.dart';
-import 'package:leagx/ui/widgets/gradient/gradient_border_widget.dart';
 import 'package:leagx/ui/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -33,68 +31,82 @@ class _FixtureScreenState extends State<FixtureScreen> {
   List<SubscribedLeague> subscribedLeagues = [];
   bool isFiltering = false;
   int selectedIndex = -1;
+  late ScrollController _scrollController;
+  final int baseScrollPoint = 3;
+  double scrollWidth = 0.0;
+  int move = 1;
 
   late DashBoardViewModel _dashBoardViewModel;
 
   @override
   Widget build(BuildContext context) {
+    _scrollController = ScrollController();
     _dashBoardViewModel = context.read<DashBoardViewModel>();
-    subscribedMatches = isFiltering == true ?  _dashBoardViewModel.filteredMatches : _dashBoardViewModel.subscribedMatches;
+    subscribedMatches = isFiltering == true
+        ? _dashBoardViewModel.filteredMatches
+        : _dashBoardViewModel.subscribedMatches;
     subscribedLeagues = _dashBoardViewModel.subscribedLeagues;
     return RefreshIndicator(
       backgroundColor: AppColors.textFieldColor,
       onRefresh: _refreshData,
       child: Column(
         children: [
+          UIHelper.verticalSpaceSmall,
+          TextWidget(
+            text: loc.dashboardFixtureTxtLeagues,
+            fontWeight: FontWeight.w700,
+          ),
+          UIHelper.verticalSpaceSmall,
           Container(
-            padding: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              gradient: AppColors.blackishGradient,
+            padding: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16.0,
             ),
+            decoration: BoxDecoration(
+                gradient: AppColors.blackishGradient,
+                borderRadius: const BorderRadius.all(Radius.circular(10))),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextWidget(
-                  text: loc.dashboardFixtureTxtLeagues,
-                  fontWeight: FontWeight.w700,
-                ),
-                UIHelper.verticalSpaceSmall,
                 Row(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(right: 18.0),
-                      child: LeagueAvatarWidget(
-                        onPressed: () async {
-                            await Navigator.of(context).pushNamed(Routes.chooseLeague, arguments: false);
+                        padding: const EdgeInsets.only(right: 18.0),
+                        child: LeagueAvatarWidget(
+                          onPressed: () async {
+                            await Navigator.of(context).pushNamed(
+                                Routes.chooseLeague,
+                                arguments: false);
                             setState(() {});
-                        },
-                        isSelected: false,
-                        width: 40,
-                        iconData: Icons.add,
-                      )
-                    ),
-                    if (subscribedLeagues.isNotEmpty) Padding(
-                      padding: const EdgeInsets.only(right: 18.0),
-                      child: LeagueAvatarWidget(
+                          },
+                          isSelected: false,
+                          width: 40,
+                          iconData: Icons.add,
+                        )),
+                    if (subscribedLeagues.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsets.only(right: 18.0),
+                        child: LeagueAvatarWidget(
                           onPressed: showAll,
                           isSelected: !isFiltering,
                           width: 40,
                           text: loc.dashboardFixtureTxtAll,
-
                         ),
-                    ),
-                    if(subscribedLeagues.isNotEmpty) Expanded(
-                      child: SizedBox(
-                        height: 40.0,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: subscribedLeagues.length,
-                          shrinkWrap: true,
-                          itemBuilder: (_, index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(right: 20.0),
-                              child: LeagueAvatarWidget(
+                      ),
+                    if (subscribedLeagues.isNotEmpty)
+                      Expanded(
+                        child: SizedBox(
+                          height: 40.0,
+                          child: ListView.builder(
+                            controller: _scrollController,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: subscribedLeagues.length,
+                            shrinkWrap: true,
+                            itemBuilder: (_, index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(right: 20.0),
+                                child: LeagueAvatarWidget(
                                   onPressed: () => filterByLeague(index),
                                   placeHolderImg: Assets.icLeague,
                                   isSelected: index == selectedIndex,
@@ -102,72 +114,78 @@ class _FixtureScreenState extends State<FixtureScreen> {
                                   padding: const EdgeInsets.all(5.0),
                                   width: 40,
                                 ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                    
+                    InkWell(
+                        onTap: () => _scrollToTheNextItemView(),
+                        child: const Icon(Icons.arrow_forward_ios_rounded))
                   ],
                 ),
               ],
             ),
           ),
-          subscribedMatches.isNotEmpty ? Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 20.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                   TextWidget(
-                    text: loc.dashboardFixtureTxtUpcomingMatches,
-                    fontWeight: FontWeight.w700,
+          subscribedMatches.isNotEmpty
+              ? Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 20.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextWidget(
+                          text: loc.dashboardFixtureTxtUpcomingMatches,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        UIHelper.verticalSpaceSmall,
+                        Expanded(
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: subscribedMatches.length,
+                              itemBuilder: (context, index) {
+                                Fixture match = subscribedMatches[index];
+                                return FixtureWidget(
+                                    key: UniqueKey(),
+                                    leagueName: match.leagueName,
+                                    teamOneFlag: match.teamHomeBadge,
+                                    teamOneName: match.matchHometeamName,
+                                    teamTwoFlag: match.teamAwayBadge,
+                                    teamTwoName: match.matchAwayteamName,
+                                    scheduledTime: match.matchTime,
+                                    scheduledDate: match.matchDate,
+                                    isLive: match.matchLive == "1",
+                                    isOver:
+                                        Utility.isMatchOver(match.matchStatus!),
+                                    matchStatus: match.matchStatus,
+                                    teamOneScore: match.matchHometeamScore,
+                                    teamTwoScore: match.matchAwayteamScore,
+                                    onTap: (leagueName) async {
+                                      bool isConnected =
+                                          await InternetInfo.isConnected();
+                                      if (isConnected == true) {
+                                        Navigator.pushNamed(
+                                            context, Routes.fixtureDetails,
+                                            arguments: MatchArgs(
+                                              matchId: match.matchId,
+                                              leagueName: leagueName,
+                                            ));
+                                      }
+                                    });
+                              }),
+                        )
+                      ],
+                    ),
                   ),
-                  UIHelper.verticalSpaceSmall,
-                  Expanded(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: subscribedMatches.length,
-                      itemBuilder: (context, index) {
-                      Fixture match = subscribedMatches[index];
-                      return FixtureWidget(
-                        key: UniqueKey(),
-                        leagueName: match.leagueName,
-                        teamOneFlag: match.teamHomeBadge,
-                        teamOneName: match.matchHometeamName,
-                        teamTwoFlag: match.teamAwayBadge,
-                        teamTwoName: match.matchAwayteamName,
-                        scheduledTime: match.matchTime,
-                        scheduledDate: match.matchDate,
-                        isLive: match.matchLive == "1",
-                        isOver: Utility.isMatchOver(match.matchStatus!),
-                        matchStatus: match.matchStatus,
-                        teamOneScore: match.matchHometeamScore,
-                        teamTwoScore: match.matchAwayteamScore,
-                        onTap: (leagueName) async {
-                          bool isConnected = await InternetInfo.isConnected();
-                          if (isConnected == true) {
-                            Navigator.pushNamed(context, Routes.fixtureDetails,
-                            arguments: MatchArgs(
-                              matchId: match.matchId,
-                              leagueName: leagueName,
-                            )
-                            );
-                          }
-                        }
-                      );
-                    }),
-                  )
-                ],
-              ),
-            ),
-          ) : Padding(
-            padding: const EdgeInsets.symmetric(horizontal:8.0),
-            child: PlaceHolderTile(height: 80, msgText: loc.dashboardFixtureTxtEmptyList),
-          ),
+                )
+              : Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: PlaceHolderTile(
+                      height: 80, msgText: loc.dashboardFixtureTxtEmptyList),
+                ),
         ],
       ),
     );
@@ -180,7 +198,7 @@ class _FixtureScreenState extends State<FixtureScreen> {
       setState(() {});
     }
   }
-  
+
   filterByLeague(int index) {
     if (selectedIndex != index) {
       _dashBoardViewModel.filterByLeague(
@@ -195,4 +213,28 @@ class _FixtureScreenState extends State<FixtureScreen> {
   Future<void> _refreshData() async {
     await _dashBoardViewModel.getAllFixtures();
   }
+
+  _scrollToTheNextItemView(
+      {ScrollDirection scrollDirection = ScrollDirection.forward}) async {
+    if (scrollDirection == ScrollDirection.forward) {
+      if (_scrollController.position.pixels <
+              _scrollController.position.maxScrollExtent &&
+          !_scrollController.position.outOfRange) {
+        await _scrollController.animateTo(
+            _scrollController.position.pixels + 100,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeOut);
+      }
+    } else {
+      if (_scrollController.position.pixels >
+              _scrollController.position.minScrollExtent &&
+          !_scrollController.position.outOfRange) {
+        await _scrollController.animateTo(
+            _scrollController.position.pixels - 100,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeOut);
+      }
+    }
+  }
 }
+enum ScrollDirection { forward, backward }
